d2bd21125f2329796c95b21dc8e8c31d9d67726b
Diego Alejandro Marín Huerta <damarin@efron.loc>
2020-04-11 17:36:59 -0500

    Se incluye Jquery 3.5.0

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 <html>
 <head>
 	<title>Alejandro Marin</title>
+	<script type="text/javascript" src="librerias/jquery.js"></script>
 </head>
 <body>
 	<h1>Esta es la segunda reunion</h1>
+	<div id="contenedor">
+  		Click aqui
+	</div>
 </body>
+	<script type="text/javascript">
+		$("#contenedor").click(function() {
+  			console.log("Hizo click");
+		});
+	</script>
 </html>
